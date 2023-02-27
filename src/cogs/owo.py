ea8684eabd5911014cc4307ecaaf6e0a89410ce0
Andrew Powers-Holmes <aholmes@omnom.net>
2023-02-28 00:04:38 +1100

    ◔w◔

--- src/cogs/owo.py
+++ src/cogs/owo.py
@@ -8,14 +8,18 @@ from owomatic.bot import Owomatic
 
 logger = logging.getLogger(__package__)
 
-TRIGGER_STRINGS = ["owo", "uwu"]
-OWO_STRINGS = ["◕w◕", "σωσ", "◔w◔", "♥w♥", "𝓞𝔀𝓞", "𝙤𝙬𝙤", "uωu", "UωU", "OωO", "oωo", "OωO", "oωo"]
+OWO_VAULT = ["◕w◕", "σωσ", "◔w◔", "♥w♥", "𝓞𝔀𝓞", "𝙤𝙬𝙤", "uωu", "UωU", "OωO", "oωo", "OωO", "oωo"]
+OWO_SENSE = ["owo", "uwu"]
+
+
+def has_owo(message: Message) -> bool:
+    return any(owo in message.content.lower() for owo in OWO_SENSE)
 
 
 class Owo(commands.Cog, name="template-slash"):
     def __init__(self, bot: Owomatic):
         self.bot = bot
-        self.owo_pool = OWO_STRINGS
+        self.owo_pool = OWO_VAULT
 
     async def cog_load(self) -> None:
         logger.info("owo? what's this?")
@@ -24,14 +28,11 @@ class Owo(commands.Cog, name="template-slash"):
     async def cog_unload(self) -> None:
         return await super().cog_unload()
 
-    def has_owo(self, message: Message) -> bool:
-        return any(trigger in message.content.lower() for trigger in TRIGGER_STRINGS)
-
     def get_owo(self):
         # if we only have one option left, use that and reset the pool
         if len(self.owo_pool) == 1:
             owo = self.owo_pool[0]
-            self.owo_pool = OWO_STRINGS
+            self.owo_pool = OWO_VAULT
         else:  # get a random owo from the pool and remove it from the pool
             owo = random_choice(self.owo_pool)
             self.owo_pool.remove(owo)
@@ -42,7 +43,7 @@ class Owo(commands.Cog, name="template-slash"):
         if (message.author.bot is True) or (message.author == self.bot.user):
             return
 
-        if self.has_owo(message):
+        if has_owo(message):
             logger.info("owo!")
             await message.reply(self.get_owo())
 
