9686df3d0a008335d93d0a50ef989f8e4c48eaf6
Andrew Powers-Holmes <aholmes@omnom.net>
2023-03-12 13:23:49 +1100

    fixup general cog

--- src/cogs/general.py
+++ src/cogs/general.py
@@ -1,36 +1,70 @@
 import platform
-import random
 
-import aiohttp
 import disnake
-from disnake import ApplicationCommandInteraction, Option, OptionType
+from disnake import ApplicationCommandInteraction
 from disnake.ext import commands
 
+from owomatic.bot import Owomatic
 from owomatic.helpers import checks
 
 
 class General(commands.Cog, name="general"):
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: Owomatic = bot
 
     @commands.slash_command(
         name="botinfo",
-        description="Get some theoretically useful information about the bot.",
+        description="Get some info about the bot.",
     )
     @checks.not_blacklisted()
-    async def botinfo(self, interaction: ApplicationCommandInteraction) -> None:
+    async def botinfo(self, ctx: ApplicationCommandInteraction) -> None:
         """
         Get some useful (or not) information about the bot.
-        :param interaction: The application command interaction.
+        :param ctx: The application command ctx.
         """
-        embed = disnake.Embed(description="a bot for WWF role management", color=0x9C84EF)
-        embed.set_author(name="Bot Info", icon_url=self.bot.user.avatar.url)
-        embed.add_field(name="Owner:", value=self.bot.config["owner"], inline=True)
-        embed.add_field(name="Running on:", value=f"Python {platform.python_version()}", inline=True)
-        embed.add_field(name="Repo:", value=self.bot.config["repo_url"], inline=False)
-        embed.add_field(name="Prefix:", value="/ (Slash Commands)", inline=False)
-        embed.set_footer(text=f"Requested by {interaction.author}", icon_url=interaction.author.avatar.url)
-        await interaction.send(embed=embed)
+        icon_url = ctx.author.display_avatar.url if ctx.author.display_avatar else ctx.author.avatar.url
+        embed = disnake.Embed(description="a questionably intelligent discord bot", color=0x9C84EF)
+        embed.set_author(name="Info", icon_url=self.bot.user.avatar.url)
+        embed.add_field(name="Owner:", value=self.bot.owner.mention, inline=True)
+        embed.add_field(name="Source repo:", value=self.bot.config.get("repo_url", "N/A"), inline=True)
+        embed.add_field(name="Running on", value=f"Python {platform.python_version()}", inline=False)
+        embed.add_field(name="Timezone:", value=str(self.bot.config.get("timezone", "Unknown")), inline=True)
+        embed.add_field(name="Uptime", value=self.bot.fuzzyuptime, inline=False)
+        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=icon_url)
+        await ctx.send(embed=embed)
+
+    @commands.slash_command(name="ping", description="ping the bot")
+    @checks.not_blacklisted()
+    async def ping(self, ctx: ApplicationCommandInteraction) -> None:
+        """
+        Check if the bot is alive.
+        :param ctx: The application command ctx.
+        """
+        embed = disnake.Embed(
+            title="🏓 Pong!",
+            description=f"Current API latency is {round(self.bot.latency * 1000)}ms",
+            color=0x9C84EF,
+        )
+        await ctx.send(embed=embed, ephemeral=True)
+
+    @commands.slash_command(
+        name="get-invite", description="Get the bot's invite link to add it to your server."
+    )
+    @checks.not_blacklisted()
+    async def invite(self, ctx: ApplicationCommandInteraction) -> None:
+        """
+        Get the invite link of the bot to be able to invite it.
+        :param ctx: The command context.
+        """
+        embed = disnake.Embed(
+            description=f"Invite me by clicking [here](https://discordapp.com/oauth2/authorize?&client_id={self.bot.config['application_id']}&scope=bot+applications.commands&permissions={self.bot.config['permissions']}).",
+            color=0xD75BF4,
+        )
+        try:
+            await ctx.author.send(embed=embed)
+            await ctx.send("Check your private messages for the link!", ephemeral=True)
+        except disnake.Forbidden:
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
