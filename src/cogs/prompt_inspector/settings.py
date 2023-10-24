03c54ba946987bc12a3f13ec34b15381dc9e1de4
Andrew Powers-Holmes <aholmes@omnom.net>
2023-10-24 17:04:23 +1100

    some PI fixup

--- src/cogs/prompt_inspector/settings.py
+++ src/cogs/prompt_inspector/settings.py
@@ -7,7 +7,7 @@ from pydantic import BaseSettings, Field
 from owomatic import DATADIR_PATH
 from owomatic.settings import GuildSettingsList, JsonConfig
 
-INPECTOR_CFG_PATH = DATADIR_PATH.joinpath("prompt-inspector.json")
+INSPECTOR_CFG_PATH = DATADIR_PATH.joinpath("prompt-inspector.json")
 
 
 class InspectorSettings(BaseSettings):
@@ -15,12 +15,12 @@ class InspectorSettings(BaseSettings):
     guilds: GuildSettingsList = Field([])
 
     class Config(JsonConfig):
-        json_config_path = INPECTOR_CFG_PATH
+        json_config_path = INSPECTOR_CFG_PATH
 
 
 @lru_cache(maxsize=2)
 def get_inspector_settings(config_path: Optional[Path] = None) -> InspectorSettings:
     if config_path is None:
-        config_path = INPECTOR_CFG_PATH
+        config_path = INSPECTOR_CFG_PATH
     settings = InspectorSettings(json_config_path=config_path)
     return settings
